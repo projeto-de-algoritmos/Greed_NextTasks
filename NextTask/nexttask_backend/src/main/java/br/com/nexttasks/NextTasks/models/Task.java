56bcb7067d7d48421ea80319d9d34c05af7228d7
Artur de Sousa <artursv2001@gmail.com>
2023-01-13 11:04:11 -0300

    criado model Task

--- NextTask/nexttask_backend/src/main/java/br/com/nexttasks/NextTasks/models/Task.java
+++ NextTask/nexttask_backend/src/main/java/br/com/nexttasks/NextTasks/models/Task.java
@@ -1,4 +1,36 @@
 package br.com.nexttasks.NextTasks.models;
 
+import java.time.LocalDate;
+
 public class Task {
+
+    private String name;
+
+    private LocalDate deadline;
+
+    private Long daysToComplete;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public Long getDaysToComplete() {
+        return daysToComplete;
+    }
+
+    public void setDaysToComplete(Long daysToComplete) {
+        this.daysToComplete = daysToComplete;
+    }
 }
